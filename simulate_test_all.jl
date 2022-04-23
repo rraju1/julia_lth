dc2a62e1540eb33af2b5d55b90a8da07118a488e
Kyle Daruwalla <daruwalla@wisc.edu>
2022-04-23 15:39:48 -0500

    Update simulate_test_all with scaling

--- simulate_test_all.jl
+++ simulate_test_all.jl
@@ -1,9 +1,11 @@
 include("src/setup.jl")
 
+BitSAD.set_saturation_verbosity(:none)
+
 ## defining the data
 
-##dataroot = "/group/ece/ececompeng/lipasti/libraries/datasets/vww-hackathon/"
-dataroot = joinpath(artifact"vww", "vww-hackathon")
+dataroot = "/group/ece/ececompeng/lipasti/libraries/datasets/vww-hackathon/"
+# dataroot = joinpath(artifact"vww", "vww-hackathon")
 testdata = VisualWakeWords(dataroot; subset = :test)
 
 ## data augmentation
@@ -12,13 +14,16 @@ testset = map_augmentation(ImageToTensor(), testdata)
 ;
 
 ## model definition and select a random sample
-##modelpath = joinpath(artifact"mobilenet", "mobilenet.bson")
-##m = BSON.load(modelpath)[:m]
 
-## mbit = BSON.load("mobilenet-bitsad.bson")[:m]
-## mbit = mbit |> tosbitstream
+modelpath = joinpath(artifact"mobilenet", "mobilenet.bson")
+m = BSON.load(modelpath)[:m]
 
-BitSAD.set_saturation_verbosity(:none)
+## make bitstream model and get scaling
+
+mbit, scaling = prepare_bitstream_model(m)
+total_scaling = prod(prod.(scaling))
+mbit = mbit |> tosbitstream
+msim = make_simulatable(mbit, (96, 96, 3, 1));
 
 ## loop through test set
 truepos = 0
@@ -28,20 +33,20 @@ falseneg = 0
 for (x, y) in testset
     local xu = Flux.unsqueeze(x, 4)
     local xbit = SBitstream.(xu)
-    local ybit = msim(xbit)
+    local ybit = mbit(xbit)
     for t in 1:100
         push!.(ybit, pop!.(msim(xbit)))
         println("y: ", y[1], "yestimate[", t, "]: ", estimate(ybit[1]))
     end
-    yestimate = estimate(ybit[1])
-    if (y[1] > 0)
-        if (yestimate > 0.5)
+    yestimate = (estimate(ybit[1]) * total_scaling > 0) ? 1 : 0
+    if y[1] == 1
+        if yestimate == 1
             global truepos += 1
         else
             global falseneg += 1
         end
     else
-        if (yestimate <= 0.5)
+        if yestimate == 0
             global trueneg += 1
         else
             global falsepos += 1
